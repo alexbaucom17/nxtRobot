2a4f0f851129b6326aefbe5b438a2cd581547cc7
Alex Baucom <alexbaucom17@gmail.com>
2017-02-19 16:34:25 -0500

    Working on core

--- src/nxt_core/src/core.py
+++ src/nxt_core/src/core.py
@@ -4,7 +4,9 @@ import nxt
 import rospy
 import tf.transformations
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
 import math
+import copy
 
 
 WHEEL_DIAM = 0.03 #meters
@@ -14,21 +16,22 @@ BASE_DIST = 0.1 #meters
 def core_node():
 
 	#configure nxt
-	motors = { 'us': nxt.PORT_A, 'left': nxt.PORT_B, 'right': nxt.PORT_C }
-	sensors = {'touch': nxt.PORT_1, 'us', nxt.PORT_4}
-	bot = robot(motors, sensors)
+	port_left =  nxt.PORT_B
+	port_right =  nxt.PORT_C
+	base = wheel_base(port_left,port_right)
 
 	#start ros node
 	rospy.init_node('nxt_core')
-	rospy.Subscriber("/cmd_vel", Twist, bot.set_vel())
-	pubUS = rospyPublisher("/scanUS", type??, queue_size = 10)
+	#rospy.Subscriber("/cmd_vel", Twist, bot.set_vel())
+	#pubUS = rospyPublisher("/scanUS", LaserScan, queue_size = 10) #TODO: confirm scan type
 
 	#loop and do stuff
 	r = rospy.Rate(10) #10 Hz
+	base.set_vel(0.1)
 	while not rospy.is_shutdown():
-		bot.update_motors()
-		scanData = bot.get_US()
-		pubUS.pub(scanData)
+		base.update_motors()
+		#scanData = bot.get_US()
+		#pubUS.pub(scanData)
 		r.sleep()
 
 class PIDcontroller:
@@ -38,7 +41,7 @@ class PIDcontroller:
 		self.P = Kp
 		self.I = Ki
 		self.D = Kd
-		self.sum+error = 0
+		self.sum_error = 0
 		self.prev_error = 0
 		self.prev_time = rospy.get_time()
 		self.motor = M #pass motor object for each tachometer data
@@ -69,12 +72,10 @@ class PIDcontroller:
 		output = self.P*error + self.I*self.sum_error + self.D*d_error
 		return output
 
-class robot:
-' Class for a wheeled robot with an actuated ultrasonic sensor for mapping '
+class wheel_base:
+' Class for a wheeled robot '
 	
-	#motors - {name: port}, valid names: left, right, us
-	#sensors - {name: port}, valid names: touch, us, light
-	def __init__(self, motors, sensors):
+	def __init__(self, left, right):
 
 		#connet to brick
 		try:
@@ -84,21 +85,14 @@ class robot:
 			return None
 
 		#set up motors
-		self.mU = nxt.Motor(self.b, motors['us'])
-		self.mL = nxt.Motor(self.b, motors['left'])
-		self.mR = nxt.Motor(self.b, motors['right'])
-
-		#set up sensors
-		self.touch = nxt.Touch(self.b, sensors['touch'])	
-		self.us = nxt.Ultrasonic(self.b, sensors['us'])
+		self.mL = nxt.Motor(self.b, port_left)
+		self.mR = nxt.Motor(self.b, port_right)
 
 		#angular speeds start at 0
-		self.w_U = 0
 		self.w_L = 0
 		self.w_R = 0
 
 		#set up PID controllers
-		self.pidU = PIDcontroller(1,0,0,self.mU)
 		self.pidL = PIDcontroller(1,0,0,self.mL)
 		self.pidR = PIDcontroller(1,0,0,self.mR)
 
@@ -107,12 +101,10 @@ class robot:
 		'update current motor speeds'
 	
 		#call PID updates
-		powU = self.pidU.update(self.w_U)
 		powL = self.pidL.update(self.w_L)
 		powR = self.pidR.update(self.w_R)
 	
 		#pass PID output to motor run method
-		self.mU.run(powU)
 		self.mL.run(powL)
 		self.mR.run(powR)			
 
@@ -134,8 +126,61 @@ class robot:
 		#convert to rad/s and store for update_motors to use
 		self.w_L = v_L/(WHEEL_DIAM/2)
 		self.w_R = v_R/(WHEEL_DIAM/2)
-		
 
+		
+class Scanner:
+
+	def __init__(self,motor_port, us_port, brick)
+		self.motor  = nxt.Motor(brick, motor_port)
+		self.US = nxt.Ultrasonic(brick, us_port)
+		self.PID = PIDcontroller(1,0,0,self.motor)
+		self.motor_speed = 1 #motor speed in rad/s
+		self.speed_factor = 4 #motor speed/sensor speed - gear ratio
+		self.scan_ready = False
+		self.scan_data  = []
+		self.old_a = 0
+		self.scan_da = 5 #how far apart scans should be in degrees
+		self.min_angle = -90
+		self.max_angle = 90
+	
+	def is_ready(self):
+		return self.scan_ready
+
+	def get_scan(self):
+		"""Get scan if data is ready"""		
+		if self.scan_ready:
+			
+			scan = LaserScan()
+			
+	
+			self.scan_ready = False		
+			self.scan_data = []
+			return scan_return
+		else:
+			return None
+
+	def update(self):
+		"""Update to keep motors running and sensor scanning"""
+		
+		#check to see if we should get another ping yet
+		cur_a = self.speed_factor * self.motor.get_tacho()
+		if abs(cur_a - self.old_a) > scan_da
+			self.scan_data.append(self.US.get_distance())
+		
+		#check to see if we need to change scan directions
+		if cur_a > self.max_angle and self.motor_speed > 0:
+			self.motor_speed = -self.motor_speed
+			self.scan_ready = True
+		if cur_a < self.min_angle and self.motor_speed < 0:
+			self.motor_speed = -self.motor_speed
+			self.scan_ready = True
+
+		#update old angle
+		self.old_a = cur_a
+
+		#compute controlled motor speed and run
+		self.motor.run(self.PID.update(self.motor_speed))
+		
 
 if __name__ == '__main__':
     try:
